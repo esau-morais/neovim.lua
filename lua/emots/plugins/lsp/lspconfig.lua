c0702016380df66deb2ec4a63ee1c38d735c7c2f
esau-morais <esaumorais7@gmail.com>
2023-10-20 19:34:42 -0300

    fix: remove typescript.nvim

--- lua/emots/plugins/lsp/lspconfig.lua
+++ lua/emots/plugins/lsp/lspconfig.lua
@@ -19,7 +19,7 @@ end
 local keymap = vim.keymap -- for conciseness
 
 -- enable keybinds only for when lsp server available
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   -- keybind options
   local opts = { noremap = true, silent = true, buffer = bufnr }
 
@@ -36,13 +36,6 @@ local on_attach = function(client, bufnr)
   keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts) -- jump to next diagnostic in buffer
   keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
   keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts) -- see outline on right hand side
-
-  -- typescript specific keymaps (e.g. rename file and update imports)
-  if client.name == "tsserver" then
-    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
-    keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
-    keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
-  end
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
